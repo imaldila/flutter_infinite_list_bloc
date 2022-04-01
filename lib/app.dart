333df3bc8a89dc47362f4b02d977f6edbd6561ba
aldilanurhadiputra@gmail.com <aldilanurhadiputra@gmail.com>
2022-04-01 15:03:31 +0700

    infinite List BloC

---
+++ lib/app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_infinite_list/posts/views/posts_page.dart';
+
+class App extends MaterialApp {
+  App() : super(home: PostsPage());
+}
